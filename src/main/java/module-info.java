ee8084453edb3b16ad3ffea49731ba9ae8dc93af
Carlos <CJAA@CJAA-PC>
2022-08-13 16:48:56 -0500

    creacion vista consulta mundiales

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module Proyecto2P_P3_G2 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens Proyecto2P_P3_G2 to javafx.fxml;
     exports Proyecto2P_P3_G2;
