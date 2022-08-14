7023a59312b8d75403c96d6e1bc3a046eee76e20
“andresACF” <andalcor@espol.edu.ec>
2022-08-13 22:10:45 -0500

    Correciones

--- src/main/java/Modelo/Partido.java
+++ src/main/java/Modelo/Partido.java
@@ -4,10 +4,121 @@
  */
 package Modelo;
 
+import Herramientas.ManejoArchivos;
+import java.util.ArrayList;
+
 /**
  *
  * @author User
  */
 public class Partido {
+
+    private String fecha;
+    private String hora;
+    private String estadio;
+    private String ciudad;
+    private String equipoLocal;
+    private String equipoVisitante;
+    private String marcador;
+    private String fase;
+    private char grupo;
+
+    public Partido(String fecha, String hora, String estadio, String ciudad, String equipoLocal, String equipoVisitante, String marcador, String fase, char grupo) {
+        this.fecha = fecha;
+        this.hora = hora;
+        this.estadio = estadio;
+        this.ciudad = ciudad;
+        this.equipoLocal = equipoLocal;
+        this.equipoVisitante = equipoVisitante;
+        this.marcador = marcador;
+        this.fase = fase;
+        this.grupo = grupo;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getEstadio() {
+        return estadio;
+    }
+
+    public void setEstadio(String estadio) {
+        this.estadio = estadio;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getEquipoLocal() {
+        return equipoLocal;
+    }
+
+    public void setEquipoLocal(String equipoLocal) {
+        this.equipoLocal = equipoLocal;
+    }
+
+    public String getEquipoVisitante() {
+        return equipoVisitante;
+    }
+
+    public void setEquipoVisitante(String equipoVisitante) {
+        this.equipoVisitante = equipoVisitante;
+    }
+
+    public String getMarcador() {
+        return marcador;
+    }
+
+    public void setMarcador(String marcador) {
+        this.marcador = marcador;
+    }
+
+    public String getFase() {
+        return fase;
+    }
+
+    public void setFase(String fase) {
+        this.fase = fase;
+    }
+
+    public char getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(char grupo) {
+        this.grupo = grupo;
+    }
     
+    public static ArrayList<Partido> cargarPartidos(String nombreArchivo){
+        ArrayList<Partido> arregloPartido=new ArrayList();
+        ArrayList<String[]> listaArreglo=ManejoArchivos.generarArreglo(nombreArchivo, "\\|");
+        for(String[] ele:listaArreglo){
+            arregloPartido.add(new Partido(ele[0].trim(),ele[1].trim(),ele[2].trim(),ele[3].trim(), ele[4].trim(),ele[5].trim(),ele[6].trim(),ele[7].trim(),ele[8].trim().charAt(0)));
+        }
+        return arregloPartido;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return "Partido{" + "fecha=" + fecha + ", hora=" + hora + ", estadio=" + estadio + ", ciudad=" + ciudad + ", equipoLocal=" + equipoLocal + ", equipoVisitante=" + equipoVisitante + ", marcador=" + marcador + ", fase=" + fase + ", grupo=" + grupo + '}';
+    }
 }
