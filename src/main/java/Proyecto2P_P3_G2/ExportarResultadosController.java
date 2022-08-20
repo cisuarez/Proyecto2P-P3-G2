b629e4c146d49d0489e867adff01696dde68a589
MichaelJimenezC <micjejim@espol.edu.ec>
2022-08-20 16:12:40 -0500

    "Creacion de metodo para cancelar y cerrar la ventana"

--- src/main/java/Proyecto2P_P3_G2/ExportarResultadosController.java
+++ src/main/java/Proyecto2P_P3_G2/ExportarResultadosController.java
@@ -10,6 +10,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -17,6 +19,8 @@ import javafx.scene.control.Alert;
  * @author Michael
  */
 public class ExportarResultadosController implements Initializable {
+    @FXML
+    Button btnCancelar;
 
     /**
      * Initializes the controller class.
@@ -35,6 +39,11 @@ public class ExportarResultadosController implements Initializable {
         alert.setTitle("Info");
         alert.setContentText("Se ha generado el archivo correctamente");
         alert.showAndWait();
+    }
+    @FXML
+    private void salir(ActionEvent e){
+        Stage s=(Stage)btnCancelar.getScene().getWindow();
+        s.close();
     }
 
 }
