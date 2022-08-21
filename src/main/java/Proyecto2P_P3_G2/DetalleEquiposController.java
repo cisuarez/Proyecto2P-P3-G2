6335e5982fd8457a0947ca04c5f11d553b84a1cb
MichaelJimenezC <micjejim@espol.edu.ec>
2022-08-21 13:31:44 -0500

    "creacion de metodo para crear imagenes de jugadores por default"

--- src/main/java/Proyecto2P_P3_G2/DetalleEquiposController.java
+++ src/main/java/Proyecto2P_P3_G2/DetalleEquiposController.java
@@ -10,10 +10,12 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 /**
@@ -24,32 +26,35 @@ import javafx.scene.layout.VBox;
 public class DetalleEquiposController implements Initializable {
 
     @FXML
-    Label Equipo1;
-    @FXML
-    Label Equipo2;
-    @FXML
-    ScrollPane jugadoresEquipo1;
-    @FXML
-    ScrollPane jugadoresEquipo2;
+    VBox root;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        System.out.println("ENTRO AL INICIAL");
-        crearContenedorJugador(4,jugadoresEquipo1,"Brazil");
-        
+        VBox crearContenedorEquipo1 = crearContenedorJugador(12, "Brazil");
+        VBox crearContenedorEquipo2 = crearContenedorJugador(14, "Alemania");
+        root.getChildren().addAll(crearContenedorEquipo1,crearContenedorEquipo2);
         // TODO
     }
 
-    private void crearContenedorJugador(int numeroDeJugadores,ScrollPane Equipo,String nombreEquipo) {
-            Equipo1.setText(nombreEquipo);       
-            System.out.println("Si sirvo");
-            VBox vbox=new VBox();
-            Label nombre= new Label();
+    private VBox crearContenedorJugador(int numeroDeJugadores, String nombreEquipo) {
+        VBox contenedorEquipo = new VBox();
+        Label equipo = new Label();
+        equipo.setText(nombreEquipo);
+        ScrollPane scrollPane = new ScrollPane();
+        HBox prueba=new HBox();
+        prueba.setSpacing(20);
+        prueba.setPrefHeight(150);
+        scrollPane.setContent(prueba);
+        for (int i = 0; i < numeroDeJugadores; i++) {
+            VBox vbox = new VBox();
+            vbox.setAlignment(Pos.CENTER);
+            
+            Label nombre = new Label();
             nombre.setText("Nombre");
-            Label jugador= new Label();
+            Label jugador = new Label();
             jugador.setText("Jugador");
             ImageView imgv = new ImageView();
             try ( FileInputStream input = new FileInputStream(Principal.pathImg + "DEFAULT.png")) {
@@ -58,11 +63,13 @@ public class DetalleEquiposController implements Initializable {
             } catch (IOException e1) {
                 System.out.println("No se encuentra la imagen");
             }
-            vbox.getChildren().addAll(imgv,nombre,jugador);
-            Equipo.setContent(vbox);
-           
-
-        
+            vbox.getChildren().addAll(imgv, nombre, jugador);
+            prueba.getChildren().add(vbox);
+            
+        }  
+       
+        contenedorEquipo.getChildren().addAll(equipo, scrollPane);
+        return contenedorEquipo;
 
     }
 
