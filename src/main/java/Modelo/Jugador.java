49a7aa4bfa0c6a853de171438844d8f37492b271
Carlos <CJAA@CJAA-PC>
2022-09-03 14:34:32 -0500

    "Comentarios para generación del javadoc"

--- src/main/java/Modelo/Jugador.java
+++ src/main/java/Modelo/Jugador.java
@@ -4,7 +4,6 @@
  */
 package Modelo;
 
-import Herramientas.ManejoArchivos;
 import Proyecto2P_P3_G2.Principal;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -12,7 +11,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
+
 
 /**
  *
@@ -23,10 +22,16 @@ public class Jugador implements Serializable {
     private String imgPath;
     private int dorsal;
     private String directorTecnico;
-    
     private String abrEquipo;
     
-
+    /**
+     *
+     * @param nombre
+     * @param imgPath
+     * @param dorsal
+     * @param directorTecnico
+     * @param abrEquipo
+     */
     public Jugador(String nombre, String imgPath, int dorsal,String directorTecnico, String abrEquipo) {
         this.nombre = nombre;
         this.imgPath = imgPath;
@@ -35,46 +40,92 @@ public class Jugador implements Serializable {
         this.abrEquipo=abrEquipo;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     *
+     * @param nombre
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getImgPath() {
         return imgPath;
     }
 
+    /**
+     *
+     * @param imgPath
+     */
     public void setImgPath(String imgPath) {
         this.imgPath = imgPath;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getDorsal() {
         return dorsal;
     }
 
+    /**
+     *
+     * @param dorsal
+     */
     public void setDorsal(int dorsal) {
         this.dorsal = dorsal;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getDirectorTecnico() {
         return directorTecnico;
     }
 
+    /**
+     *
+     * @param directorTecnico
+     */
     public void setDirectorTecnico(String directorTecnico) {
         this.directorTecnico = directorTecnico;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getAbrEquipo() {
         return abrEquipo;
     }
 
+    /**
+     *
+     * @param abrEquipo
+     */
     public void setAbrEquipo(String abrEquipo) {
         this.abrEquipo = abrEquipo;
     }
     
+    /**
+     * Este método lee el archivo que contiene la información de los jugadores
+     * y con dicha información crea los objetos de tipo Jugador que son añadidos
+     * a una lista que finalmente es retornada.
+     * @return
+     */
     public static ArrayList<Jugador> cargarJugadores(){
         ArrayList<Jugador> jugadores=new ArrayList<>();
         try(BufferedReader bf=new BufferedReader(new FileReader(Principal.pathFiles+"WorldCupPlayersBrasil2014.csv"))){
@@ -103,6 +154,11 @@ public class Jugador implements Serializable {
         return jugadores;
     }
 
+    /**
+     * Se sobreescribe el método toString para que este retorne el nombre del 
+     * jugador
+     * @return
+     */
     @Override
     public String toString() {
         return nombre;
