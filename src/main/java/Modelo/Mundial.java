2875187daa7ccf8b68fa61f4ee834182f39ff1ec
Carlos <CJAA@CJAA-PC>
2022-09-03 14:34:17 -0500

    "Comentarios para generación del javadoc"

--- src/main/java/Modelo/Mundial.java
+++ src/main/java/Modelo/Mundial.java
@@ -21,6 +21,15 @@ public class Mundial {
     private int partidosJugados;
     private ArrayList<Equipo> finalistas;
 
+    /**
+     *
+     * @param año
+     * @param golesAnotados
+     * @param cantidadEquipos
+     * @param cantidadAsistencia
+     * @param partidosJugados
+     * @param finalistas
+     */
     public Mundial(int año, int golesAnotados, int cantidadEquipos, String cantidadAsistencia, int partidosJugados, ArrayList<Equipo> finalistas) {
         this.año = año;
         this.golesAnotados = golesAnotados;
@@ -30,10 +39,17 @@ public class Mundial {
         this.finalistas = finalistas;
     }
 
-    public static ArrayList<Mundial> cargarMundiales(String nombreArchivo) {
+    /**
+     * Este método recibe carga toma datos de los mundiales del archivo correspondiente y de los equipos y 
+     * sus abreviaturas, con esos datos creas los equipos que quedaron en las primeras posiciones de cada mundial 
+     * y los instancia junto con el resto de atributos que posee el objeto de tipo Mundial. Finalmente retorna una lista
+     * de mundiales.
+     * @return
+     */
+    public static ArrayList<Mundial> cargarMundiales() {
         ArrayList<String[]> abreviaturasEquipos=ManejoArchivos.generarArreglo(Principal.pathFiles+"Abreviaturas.csv",",");
         ArrayList<Mundial> arregloMundial = new ArrayList();
-        ArrayList<String[]> listaArreglo = ManejoArchivos.generarArreglo(nombreArchivo, ",");
+        ArrayList<String[]> listaArreglo = ManejoArchivos.generarArreglo(Principal.pathFiles+"WorldCups.csv", ",");
         for (String[] ele : listaArreglo) {
             ArrayList<Equipo> finalistas = new ArrayList<>();
             for (int i = 2; i < 6; i++) {
@@ -55,50 +71,98 @@ public class Mundial {
         return arregloMundial;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getAño() {
         return año;
     }
 
+    /**
+     *
+     * @param año
+     */
     public void setAño(int año) {
         this.año = año;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getGolesAnotados() {
         return golesAnotados;
     }
 
+    /**
+     *
+     * @param golesAnotados
+     */
     public void setGolesAnotados(int golesAnotados) {
         this.golesAnotados = golesAnotados;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getCantidadEquipos() {
         return cantidadEquipos;
     }
 
+    /**
+     *
+     * @param cantidadEquipos
+     */
     public void setCantidadEquipos(int cantidadEquipos) {
         this.cantidadEquipos = cantidadEquipos;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getCantidadAsistencia() {
         return cantidadAsistencia;
     }
 
+    /**
+     *
+     * @param cantidadAsistencia
+     */
     public void setCantidadAsistencia(String cantidadAsistencia) {
         this.cantidadAsistencia = cantidadAsistencia;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getPartidosJugados() {
         return partidosJugados;
     }
 
+    /**
+     *
+     * @param partidosJugados
+     */
     public void setPartidosJugados(int partidosJugados) {
         this.partidosJugados = partidosJugados;
     }
 
+    /**
+     *
+     * @return
+     */
     public ArrayList<Equipo> getFinalistas() {
         return finalistas;
     }
 
+    /**
+     *
+     * @param finalistas
+     */
     public void setFinalistas(ArrayList<Equipo> finalistas) {
         this.finalistas = finalistas;
     }
