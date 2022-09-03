170ca6f4dab8a9ac86880bdab8b86a47e5af0d2b
Carlos <CJAA@CJAA-PC>
2022-09-03 17:12:46 -0500

    "Se agregó la clase Starter"

---
+++ src/main/java/Proyecto2P_P3_G2/Starter.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Proyecto2P_P3_G2;
+
+/**
+ *
+ * @author CJAA
+ */
+public class Starter {
+    public static void main(String[] args) {
+        Principal.main(args);
+    }
+}
